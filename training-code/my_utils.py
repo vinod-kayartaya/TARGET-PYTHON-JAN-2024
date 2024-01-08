41752fb1949dc0d21fd88fb6ab8b1019fad14419
Vinod Kumar Kayartaya <vinod@vinod.co>
2024-01-08 13:52:23 +0530

    session3

---
+++ training-code/my_utils.py
@@ -0,0 +1,33 @@
+def is_leap(year: int) -> bool:
+    return (year % 400 == 0) or (year % 100 != 0 and year % 4 == 0)
+
+
+def max_days(month: int, year: int) -> int:
+    if 1 <= month <= 12:
+        if year > 0:
+            if month == 2:
+                return 29 if is_leap(year) else 28
+            elif month in (4, 6, 9, 11):
+                return 30
+            return 31
+        else:
+            raise ValueError('year must be > 0')
+    else:
+        raise ValueError('month must be between 1 and 12')
+
+
+def factorial(num: int) -> int:
+    if type(num) is not int:
+        raise TypeError(f'num must be int, but got {type(num)}')
+    if num < 0:
+        raise ValueError('num must be > 0')
+    f = 1
+    while num:
+        f *= num
+        num -= 1
+    return f
+
+
+if __name__ == '__main__':
+    print(max_days(1, 2024))
+    print(factorial(10))
