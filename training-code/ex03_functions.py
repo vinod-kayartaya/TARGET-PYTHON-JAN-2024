0b1c7d82ca073d4b3a83f0ee922667d2ff53484f
Vinod Kumar Kayartaya <vinod@vinod.co>
2024-01-08 12:28:49 +0530

    session 2

--- training-code/ex03_functions.py
+++ training-code/ex03_functions.py
@@ -4,6 +4,9 @@ this module contains examples for user defined functions
 
 
 def greet(name: str = 'friend', city: str = 'your city') -> None:
+    if type(name) is not str or type(city) is not str:
+        raise TypeError('name and city must be str')
+
     print(f'Hello {name}! How is weather in {city}?')
 
 
@@ -15,10 +18,19 @@ def say_hello() -> None:
     print('Hello, world!')
 
 
-if __name__ == '__main__':
-    say_hello()
-    greet('Vinod', 'Bangalore')
-    greet(city='Nagpur', name='Nived')
-    greet('Uday')
-    greet()
+def add(*numbers) -> float:
+    total = 0
+    for each_num in numbers:
+        if type(each_num) in (int, float):
+            total += each_num
+    return total
 
+
+if __name__ == '__main__':
+    # say_hello()
+    # greet('Vinod', 'Bangalore')
+    # greet(city='Nagpur', name='Nived')
+    # greet('Uday')
+    # greet(10)
+    n = add(10, 20.9, 'vinod', False, 30)  # you may pass any number of arguments
+    print(f'n is {n}')
